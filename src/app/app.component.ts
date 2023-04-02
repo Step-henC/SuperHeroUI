85435d8787c5caa535a28f717f24de1eedb9a848
Stephen Cunningham <stevecunningham@outlook.com>
2023-04-01 21:07:28 -0500

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { SuperHeroService } from './services/super-hero.service';
+import { SuperHero } from './models/super-hero';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'SuperHero.UI';
+  heroes: SuperHero[] = [];
+
+
+  constructor(private superHeroService: SuperHeroService) {}
+  
+  ngOnInit() : void {
+    this.heroes = this.superHeroService.getSuperHeroes();
+    console.log(this.heroes);
+  }
+}

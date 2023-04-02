2198e2280f7a3be27bb3bbc2b6f36cf7dea319af
Stephen Cunningham <stevecunningham@outlook.com>
2023-04-02 11:09:47 -0500

    beginning api

--- src/app/services/super-hero.service.ts
+++ src/app/services/super-hero.service.ts
@@ -1,22 +1,31 @@
 import { Injectable } from '@angular/core';
 import { SuperHero } from '../models/super-hero';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SuperHeroService {
 
-  constructor() { }
+  private url ="SuperHero";
+  constructor(private http: HttpClient) { }
 
 
-  public getSuperHeroes() : SuperHero[] {
-    let hero = new SuperHero();
-    hero.id = 1;
-    hero.name = "Spider Man";
-    hero.firstName = "Peter";
-    hero.lastName = "Parker";
-    hero.place = "New York City"
+  public getSuperHeroes() : Observable<SuperHero[]> {
 
-    return [hero];
+    return this.http.get<SuperHero[]>(`${environment.apiUrl}/${this.url}`)
+
+
+    // no longer need to hard code hero with http client calling our UI
+    // let hero = new SuperHero();
+    // hero.id = 1;
+    // hero.name = "Spider Man";
+    // hero.firstName = "Peter";
+    // hero.lastName = "Parker";
+    // hero.place = "New York City"
+
+    //return [hero];
   }
 }
